15f00b49baabd1d5b023044db4930ea591e55bdd
jesimonbarreto <jesimonbarreto@gmail.com>
2021-01-16 19:10:12 -0300

    Add shake

--- vns.py
+++ vns.py
@@ -1,9 +1,12 @@
 import numpy as np
 import os,sys,math, copy
 from scipy.spatial import distance
+from random import seed,random,randint
+seed(1234)
+np.random.seed(1234)
 
-
-
+limits_ap_x = (0,100) #grid x 0 - 100 valores possiveis para um ponto de acesso
+limits_ap_y = (0,100) #grid y 0 - 100
 C = [] #conjunto de clientes (x,y)
 cc = [] #consumo do cliente i
 P = [] #conjunto de pontos(x,y) de possiveis locais para AP
@@ -13,8 +16,6 @@ N = 0.95 # taxa de cobertura dos clientes
 n_max = 100 #numero maximo de PAs disponiveis
 ap = [] # vetor len == numero de PAs e binario para indicar se a PA é usada ou nao
 acp = [[]]#Matriz de numero de clientes por numero de PAs indica se a PA é utilizada pelo cliente
-
-#Code 
 
 #função que será otimizada
 def f1():
@@ -75,6 +76,23 @@ def rest7():
             dist += distance.euclidean(c, p) - rp
     return dist
 
+def novoLocalPontoAcesso(ponto):
+    novo_x = randint(limits_ap_x[0], limits_ap_x[1])
+    novo_y = randint(limits_ap_y[0], limits_ap_y[1])
+    AP[ponto] = np.array([novo_x, novo_y])
+
+def novoPontoAcessoClient():
+    
+
+def usoPontoAcesso(n):
+    ap[n] = int(not ap[n])
+
+def usoPontoAcesso():
+    indices_one = ap == 1
+    indices_zero = ap == 0
+    ap[indices_one] = 0
+    ap[indices_zero] = 1
+
 
 #retorna um valor inicial de x aleatório e factível
 def initialSol():
@@ -82,8 +100,27 @@ def initialSol():
 
 #pertubação do domínio de solução
 def shake(x, k):
-    pass
-
+    #Estruturas de vizinhaças
+    #1 - Coloca o ponto de acesso em um novo ponto aleatorio 
+    #2 - Trocar um cliente para outro ponto de acesso aleatorio 
+    #3 - Trocar o estado de uso de um ponto de acesso aleatorio
+    #4 - Altera o estado de uso de todos os pontos de acesso
+    if k == 1:
+        #Sorteia um dos pontos de acesso que está sendo usado
+        ponto = False
+        while(not ponto):
+            p = np.random.choice(ap.shape[0], size=1, replace=False)
+            ponto = ap[p]
+        novoLocalPontoAcesso(p)
+    elif k == 2:
+        
+        novoPontoAcessoClient(pontovelho, cliente, pontonovo)
+    elif k == 3:
+        p = np.random.choice(ap.shape[0], size=1, replace=False)
+        usoPontoAcesso(p)
+    elif k == 4:
+        usoPontoAcesso()
+    
 def neighborhoodChange(x, x_line, k, f):
     
     if f(x_line) < f(x):
