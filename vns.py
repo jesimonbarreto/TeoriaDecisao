b2b307ef16b3ff2b9231672e8c6fcec1336d2437
jesimonbarreto <jesimonbarreto@gmail.com>
2021-01-16 13:55:09 -0300

    Add constraints functions

--- vns.py
+++ vns.py
@@ -1,10 +1,69 @@
 import numpy as np
 import os,sys,math, copy
-
+from scipy.spatial import distance
 
 #função que será otimizada
 def f():
     pass
+
+C = [] #conjunto de clientes (x,y)
+cc = [] #consumo do cliente i
+P = [] #conjunto de pontos(x,y) de possiveis locais para AP
+cp = 150 #capacidade do ponto de acesso p
+rp = 85 # limite do sinal do ponto de acesso
+N = 0.95 # taxa de cobertura dos clientes
+n_max = 100 #numero maximo de PAs disponiveis
+ap = [] # vetor len == numero de PAs e binario para indicar se a PA é usada ounao
+acp = [[]]#Matriz de numeto de clientes por numero de PAs indica se a PA é utilizada pelo cliente
+
+
+def f2():
+    #Calcula o valor da função objetivo f2
+    f2_value = 0
+    for id_c,c in enumerate(C):
+        for id_p,p in enumerate(P):
+            f2_value += distance.euclidean(c, p) * acp[id_c][id_p]
+    
+    #restrições
+#atender N % dos clientes
+def rest3():
+    total_clients_att = 0
+    for id_c,c in enumerate(C):
+        for id_p,p in enumerate(P):
+            total_clients_att+= acp[id_c][id_p]
+    
+    penal = N * len(C) - total_clients_att
+
+    return penal
+
+#Garante nao estourar a capacidade do ponto de acesso
+def rest4():
+    pontos_cap = 0 
+    for id_c,c in enumerate(C):
+        pontos_cap += acp[id_c] * cc
+    
+    penal = pontos_cap - cp
+    return penal
+#garante que cada cliente estará conectado a 1 AP
+def rest5():
+    pert = [] 
+    for id_p,p in enumerate(P):
+        ac = np.sum(acp[:,id_p])
+        pert.append(ac-1)
+    penal = np.sum(pert)
+    return penal
+
+def rest6():
+    penal = np.sum(ap) - n_max
+    return penal
+
+def rest7():
+    dist = 0
+    for id_c,c in enumerate(C):
+        for id_p,p in enumerate(P):
+            dist += distance.euclidean(c, p) - rp
+    return dist
+
 
 #retorna um valor inicial de x aleatório e factível
 def initialSol():
