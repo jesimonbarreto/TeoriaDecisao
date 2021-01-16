1b9eae268c9003c3ef78427e8169870458b0a28f
Daniel Piçarro <daniel.picarro@gmail.com>
2021-01-16 16:33:10 -0300

    added gitignore
    Issue:
    
    # Please use the following guidelines to format all your commit messages:
    #
    #    <subject>
    #    Issue: <issue reference>
    #    <BLANK LINE>
    #    <optional comment>
    #
    # Example:
    #    Adding commit template message
    #    Issue: OmniLogic/Operations#1000 or AB#26
    #
    #    This commit contains the git commit template
    #
    # Thank you \o/

--- vns.py
+++ vns.py
@@ -1,76 +1,78 @@
+import copy
+
 import numpy as np
-import os,sys,math, copy
 
 
-#função que será otimizada
+# função que será otimizada
 def f():
     pass
 
-#retorna um valor inicial de x aleatório e factível
+
+# retorna um valor inicial de x aleatório e factível
 def initialSol():
     pass
 
-#pertubação do domínio de solução
+
+# pertubação do domínio de solução
 def shake(x, k):
     x_per = copy.copy(x)
     n = len(x)
-    #permuta a posição
+    # permuta a posição
     r = np.random.permutation(n)
 
-    if k == 1:      # exchange two random positions
+    if k == 1:  # exchange two random positions
         x_per[r[1]] = x[r[2]]
         x_per[r[2]] = x[r[1]]
-    elif k == 2:    #exchage three random positions
+    elif k == 2:  # exchage three random positions
         x_per[r[1]] = x[r[2]]
         x_per[r[2]] = x[r[3]]
-        x_per[r[3]] = x[r[1]]    
-    elif k == 3:     # shift positions
+        x_per[r[3]] = x[r[1]]
+    elif k == 3:  # shift positions
         if r[1] < r[2]:
             r1, r2 = r[1], r[2]
         else:
             r1, r2 = r[2], r[1]
 
-        x_per = [x_per[1:r1-1], x_per[r1+1:r2], x_per[r1], x_per[r2+1:]]
+        x_per = [x_per[1:r1 - 1], x_per[r1 + 1:r2], x_per[r1], x_per[r2 + 1:]]
 
-    return x_pert   
+    return x_pert
+
 
 def neighborhoodChange(x, x_line, k, f):
-    
     if f(x_line) < f(x):
-        x  = x_line
+        x = x_line
         k = 1
     else:
-        k  += 1
-    
-    return x, k
-    
+        k += 1
 
-#k_max Número de estruturas de vizinhaças definidas
-#max_int numero maximo de tentativas de novos valores
-def VNS(k_max = 3, max_int = 5000):
-    
+    return x, k
+
+
+# k_max Número de estruturas de vizinhaças definidas
+# max_int numero maximo de tentativas de novos valores
+def VNS(k_max=3, max_int=5000):
     nfe = 0
     x_save = []
     y_save = []
-    
+
     # Solução inicial
     x = initialSol()
     x_save.append(x)
     y_save.append(f(x))
-    
-    while (nfe<=max_int):
-        
+
+    while (nfe <= max_int):
+
         k = 1
-        while(k<k_max):
+        while (k < k_max):
             # Gera uma solu��o na k-�sima vizinhan�a de x
-            x_line = shake(x,k)
-            #update x
+            x_line = shake(x, k)
+            # update x
             x, k = neighborhoodChange(x, x_line, k, f)
-            #save 
+            # save
             x_save.append(x)
             y_save.append(f(x))
-            nfe +=1
-        
+            nfe += 1
+
     x_sol = x
 
     return x_sol, x_save, y_save
